0d7a7453684f255e0d38f45fdc0813b10117eb2d
Brittany Storoz <brittanystoroz@gmail.com>
2014-11-14 06:55:36 -0500

    Stub data store and redirect to clock route

---
+++ app/routes/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model: function() {
+        return this.store;
+    }, 
+    redirect: function() {
+        this.transitionTo('clock');
+    }
+});
