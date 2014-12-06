3c1ca7824649bbccf6be8eea3cf93495ab7b0f82
Brittany Storoz <brittanystoroz@gmail.com>
2014-12-06 12:12:42 -0800

    Update timezones template

--- app/controllers/clock.js
+++ app/controllers/clock.js
@@ -5,16 +5,22 @@ export default Ember.Controller.extend({
         // Update the time.
         this.updateTime();
     },
- 
+
     updateTime: function() {
         var _this = this;
  
         // Update the time every second.
         Ember.run.later(function() {
-            _this.set('localTime', new Date().toLocaleTimeString());
+            _this.set('localTime', moment().format('h:mm:ss a'));
+ 
+            _this.get('model').forEach(function(model) {
+                model.set('time',
+                          moment().tz(model.get('name')).format('h:mm:ss a'));
+            });
+ 
             _this.updateTime();
         }, 1000);
-    },
- 
-    localTime: new Date().toLocaleTimeString()
+    }.on('init'),
+
+   localTime: moment().format('h:mm:ss a')
 });
