0c5f95e7353cba9b6ef4c1936f40890bfe4b0b50
Brittany Storoz <brittanystoroz@gmail.com>
2014-12-06 11:57:41 -0800

    Add updateTime method to clock controller

--- app/controllers/clock.js
+++ app/controllers/clock.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  localTime: new Date().toLocaleTimeString()
+    init: function() {
+        // Update the time.
+        this.updateTime();
+    },
+ 
+    updateTime: function() {
+        var _this = this;
+ 
+        // Update the time every second.
+        Ember.run.later(function() {
+            _this.set('localTime', new Date().toLocaleTimeString());
+            _this.updateTime();
+        }, 1000);
+    },
+ 
+    localTime: new Date().toLocaleTimeString()
 });
